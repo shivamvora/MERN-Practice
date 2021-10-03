ca632ad2d2f2fec04f4ef9e69950c68747684983
shivamvora <shivampatel27102001@gmail.com>
2021-10-03 16:39:42 +0530

    adding value into db's collection

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,15 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
+require('../db/conn');
+const User = require('../model/userSchema');
+
+
 router.get( '/', ( req, res ) => {
     res.send( 'Hello world from the auth' )
 } );
 
 router.post('/register',(req,res)=>{
-    console.log(req.body);
-    res.json({message:req.body});
-    res.send("my register page");
-})
 
+    const {name,email,phone,work,password,cpassword}=req.body;
+    if(!name || !email || !phone || !work || !password || !cpassword){
+        return res.status(422).json({error:"please fillled the field properly"})
+    }    
+
+
+
+User.findOne({email:email})
+.then((userExist)=>{
+    if(userExist){
+        return res.status(422).json({error:"Email already Exist"})
+    }
+    const user = new User({name,email,phone,work,password,cpassword});
+
+    user.save().then(()=>{
+        res.status(201).json({message:"user registered successfully"})
+    }).catch((err)=>res.status(500).json({error:"failed to registered"}))
+}).catch(err=>{console.log(err)})
+});
 module.exports = router;
 
