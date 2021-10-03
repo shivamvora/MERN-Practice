bb0b5ef2d472f3d9b6cf039d1771dd0d8b72a8b6
shivamvora <shivampatel27102001@gmail.com>
2021-10-03 19:25:41 +0530

    changes for password bcrypt and mathing password then and then only should be register

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -1,38 +1,48 @@
-const mongoose = require('mongoose');
-
+const mongoose = require( 'mongoose' );
+const bcrypt = require( 'bcryptjs' );
 /**
  * defines the userSchema
  */
 
-const userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+const userSchema = new mongoose.Schema( {
+    name: {
+        type: String,
+        required: true
     },
-    email:{
-        type:String,
-        required:true
+    email: {
+        type: String,
+        required: true
     },
-    phone:{
-        type:Number,
-        required:true
+    phone: {
+        type: Number,
+        required: true
     },
-    work:{
-        type:String,
-        required:true
+    work: {
+        type: String,
+        required: true
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
     },
-    cpassword:{
-        type:String,
-        required:true
+    cpassword: {
+        type: String,
+        required: true
     }
-})
+} )
 
 
-const User = mongoose.model('USER',userSchema);
+
+// hashing the password
+userSchema.pre( 'save', async function ( next ) {
+    if ( this.isModified( 'password' ) ) {
+        this.password = await bcrypt.hash( this.password, 12 );
+        this.cpassword = await bcrypt.hash( this.cpassword, 12 );
+    }
+    next();
+} )
 
 
+
+const User = mongoose.model( 'USER', userSchema );
 module.exports = User;
--- server/router/auth.js
+++ server/router/auth.js
@@ -47,11 +47,14 @@ router.post( '/register', async ( req, res ) => {
         const userExist = await User.findOne( { email: email } );
         if ( userExist ) {
             return res.status( 422 ).json( { error: "Email already Exist" } )
-        }
-        const user = new User( { name, email, phone, work, password, cpassword } );
-        const userRegister = await user.save();
-        if ( userRegister ) {
-            res.status( 201 ).json( { message: "user registered successfully" } )
+        } else if ( password != cpassword ) {
+            return res.status( 422 ).json( { error: "Password are not matching" } )
+        } else {
+            const user = new User( { name, email, phone, work, password, cpassword } );
+            const userRegister = await user.save();
+            if ( userRegister ) {
+                res.status( 201 ).json( { message: "user registered successfully" } )
+            }
         }
     } catch ( err ) {
         console.log( err )
@@ -63,22 +66,22 @@ router.post( '/register', async ( req, res ) => {
  * Login Route
  */
 
-router.post('/signin',async (req,res)=>{
-    try{
-        const {email,password} = req.body;
-        if(!email || !password){
-            return res.status(400).json({error:"Please filled the data"})
+router.post( '/signin', async ( req, res ) => {
+    try {
+        const { email, password } = req.body;
+        if ( !email || !password ) {
+            return res.status( 400 ).json( { error: "Please filled the data" } )
         }
-        const userLogin = await User.findOne({email:email});
-        if(!userLogin){
-            res.status(400).json({message:"user Error Successfully"})
-        }else{
-        res.json({message:"user Sigin Successfully"})
+        const userLogin = await User.findOne( { email: email } );
+        if ( !userLogin ) {
+            res.status( 400 ).json( { message: "user Error Successfully" } )
+        } else {
+            res.json( { message: "user Sigin Successfully" } )
         }
-    }catch(err){
-        console.log(err);
+    } catch ( err ) {
+        console.log( err );
     }
-})
+} )
 
 
 module.exports = router;
